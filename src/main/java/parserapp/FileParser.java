cb49dc6c0d9d9dfeec470c2aaaeeb3a99cb1cd66
Gustavo Nogueira <gustavonr.13@gmail.com>
2021-10-18 03:38:04 -0300

    testando classe FileParser por triangulação

--- src/main/java/parserapp/FileParser.java
+++ src/main/java/parserapp/FileParser.java
@@ -7,7 +7,41 @@ import java.io.IOException;
 public class FileParser {
 
     public void parse(String inputFile, String outputFile, Character delimiter, int orientation) throws IOException {
-        String content = "1;2;3\n456;523;458\n782;861;973\n755;770;753\n729;783;758\n761;720;783\n751;813;749\n869;750;794\n823;726;769\n847;770\n730;771\n";
+        String content = "";
+        
+        Reader reader = new Reader(inputFile);
+
+        String values[][] = reader.readFile();
+        int rows = reader.getRowSize();
+        int cols = reader.getColSize();
+
+        if (orientation == Constants.ROW_ORIENTATION) {
+            for (int i = 0;i < rows;i++) {
+                content += (i + 1); // identifiers
+                content += delimiter; 
+                int j = 0;
+                while(true) {
+                    content += values[i][j++];
+                    if (values[i][j] == null) break;
+                    content += delimiter;
+                }
+                content += "\n";
+            }
+        } else if (orientation == Constants.COLUMN_ORIENTATION) {
+            for (int i = 0;i < rows;i++) { // identifiers
+                content += (i + 1);
+                if (i != rows - 1) content += delimiter;
+            }
+            content += "\n";
+            for (int j = 0;j < cols;j++) {
+                for (int i = 0;i < rows;i++) {
+                    if (values[i][j] != null) content += values[i][j];
+                    if (i != rows - 1 && values[i + 1][j] != null) content += delimiter;
+                }
+                content += "\n";
+            }
+        }
+
         FileWriter writer = new FileWriter(outputFile);
         writer.write(content);
         writer.close();
--- src/test/java/parserapp/FileParserTest.java
+++ src/test/java/parserapp/FileParserTest.java
@@ -32,4 +32,26 @@ public class FileParserTest {
         File actual = new File(outputPath);
         FileAssert.assertEquals("Saída diferente da esperada.", expected, actual);
     } 
+
+    @Test
+    public void testParser2() throws IOException {
+        String inputPath = TEST_FILES_PATH + "inputTest2.out";
+        String outputPath = TEST_FILES_PATH + "outputTest2.out";
+        String expectedPath = TEST_FILES_PATH + "expectedTest2.out";
+        parser.parse(inputPath, outputPath, ';', Constants.ROW_ORIENTATION);
+        File expected = new File(expectedPath);
+        File actual = new File(outputPath);
+        FileAssert.assertEquals("Saída diferente da esperada.", expected, actual);
+    } 
+
+    @Test
+    public void testParser3() throws IOException {
+        String inputPath = TEST_FILES_PATH + "inputTest3.out";
+        String outputPath = TEST_FILES_PATH + "outputTest3.out";
+        String expectedPath = TEST_FILES_PATH + "expectedTest3.out";
+        parser.parse(inputPath, outputPath, '|', Constants.COLUMN_ORIENTATION);
+        File expected = new File(expectedPath);
+        File actual = new File(outputPath);
+        FileAssert.assertEquals("Saída diferente da esperada.", expected, actual);
+    } 
 }
