6c081472c5c24289c958f3771751588da0d8b936
Mexazonic <brunnohenriq697@gmail.com>
2021-10-18 21:26:30 -0300

    Aprimoramento Menu

--- src/main/java/parserapp/Menu.java
+++ src/main/java/parserapp/Menu.java
@@ -26,101 +26,104 @@ public class Menu extends JFrame implements ActionListener {
 	public int orientation; // linha / coluna
 	
 	public Menu() {
-			setTitle("Janela Menu");
-	        setBounds(150, 45, 450, 300);
-	        setDefaultCloseOperation(EXIT_ON_CLOSE);
-	        setResizable(true);
-	        c = getContentPane();
-	        c.setBackground(Color.pink); // Outubro Rosa
-	        c.setLayout(null);
+		setTitle("Janela Menu");
+	    setBounds(150, 45, 450, 300);
+	    setDefaultCloseOperation(EXIT_ON_CLOSE);
+	    setResizable(true);
+	    c = getContentPane();
+	    c.setBackground(Color.pink); // Outubro Rosa
+	    c.setLayout(null);
 	 
-	        title = new JLabel("Menu");
-	        title.setFont(new Font("Arial", Font.PLAIN, 18));
-	        title.setSize(80, 50);
-	        title.setLocation(200, 5);
-	        c.add(title);
+	    title = new JLabel("Menu");
+	    title.setFont(new Font("Arial", Font.PLAIN, 18));
+	    title.setSize(80, 50);
+	    title.setLocation(200, 5);
+	    c.add(title);
 	 
-	        delimitador = new JLabel("Delimitador");
-	        delimitador.setFont(new Font("Arial", Font.PLAIN, 13));
-	        delimitador.setSize(100, 20);
-	        delimitador.setLocation(90, 50);
-	        c.add(delimitador);
+	    delimitador = new JLabel("Delimitador");
+	    delimitador.setFont(new Font("Arial", Font.PLAIN, 13));
+	    delimitador.setSize(100, 20);
+	    delimitador.setLocation(90, 50);
+	    c.add(delimitador);
 	        
-	        tdelimitador = new JTextField();
-	        tdelimitador.setFont(new Font("Arial", Font.PLAIN, 15));
-	        tdelimitador.setSize(50, 20);
-	        tdelimitador.setLocation(180, 50);
-	        c.add(tdelimitador);
+	    tdelimitador = new JTextField();
+	    tdelimitador.setFont(new Font("Arial", Font.PLAIN, 15));
+	    tdelimitador.setSize(50, 20);
+	    tdelimitador.setLocation(180, 50);
+	    c.add(tdelimitador);
 	        
-	        linha = new JRadioButton("Linha");
-	        linha.setFont(new Font("Arial", Font.PLAIN, 15));
-	        linha.setSelected(false);
-	        linha.setSize(75, 20);
-	        linha.setLocation(90, 125);
-	        c.add(linha);
+	    linha = new JRadioButton("Linha");
+	    linha.setFont(new Font("Arial", Font.PLAIN, 15));
+	    linha.setSelected(false);
+	    linha.setSize(75, 20);
+	    linha.setLocation(90, 125);
+		linha.addActionListener(this);
+	    c.add(linha);
 	 
-	        coluna = new JRadioButton("Coluna");
-	        coluna.setFont(new Font("Arial", Font.PLAIN, 15));
-	        coluna.setSelected(false);
-	        coluna.setSize(80, 20);
-	        coluna.setLocation(90, 90);
-	        c.add(coluna);
-	        setVisible(true);
+	    coluna = new JRadioButton("Coluna");
+	    coluna.setFont(new Font("Arial", Font.PLAIN, 15));
+	    coluna.setSelected(false);
+	    coluna.setSize(80, 20);
+	    coluna.setLocation(90, 90);
+		coluna.addActionListener(this);
+	    c.add(coluna);
 
-	        sub = new JButton("Submit");
-	        sub.setFont(new Font("Arial", Font.PLAIN, 15));
-	        sub.setSize(100, 20);
-	        sub.setLocation(100, 220);
-	        sub.addActionListener(this);
-	        c.add(sub);
+	    sub = new JButton("Submit");
+	    sub.setFont(new Font("Arial", Font.PLAIN, 15));
+	    sub.setSize(100, 20);
+	    sub.setLocation(100, 220);
+	    sub.addActionListener(this);
+	    c.add(sub);
 	 
-	        reset = new JButton("Reset");
-	        reset.setFont(new Font("Arial", Font.PLAIN, 15));
-	        reset.setSize(100, 20);
-	        reset.setLocation(240, 220);
-	        reset.addActionListener(this);
-	        c.add(reset);
+	    reset = new JButton("Reset");
+	    reset.setFont(new Font("Arial", Font.PLAIN, 15));
+	    reset.setSize(100, 20);
+	    reset.setLocation(240, 220);
+	    reset.addActionListener(this);
+	    c.add(reset);
+
+		setVisible(true);
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == sub) {
 			opDelimitador = tdelimitador.getText();
-			
-			if (linha.isSelected()) {
-		        coluna.setSelected(false);
-				orientation = Constants.ROW_ORIENTATION;
-			} else {
-				orientation = Constants.COLUMN_ORIENTATION;
-                linha.setSelected(false);
-            }
-			try {
-					ValidacaoDelimitador(opDelimitador);
-				} catch (DelimitadorInvalidoException e1) {
-					e1.printStackTrace();
-				}
-				retornoDelimitador();
-				retornoOrientacao();
-
-				FileParser parser = new FileParser();
-				Character delimiter = opDelimitador.charAt(0);
-				try {
-					parser.parse(Constants.FILE_PATH + "analysisMemory.out", Constants.FILE_PATH + "analysisMemoryTab.out", delimiter, orientation);
-					parser.parse(Constants.FILE_PATH + "analysisTime.out", Constants.FILE_PATH + "analysisTimeTab.out", delimiter, orientation);
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-            }
 		
-		else if (e.getSource() == reset) {
+			try {
+				ValidacaoDelimitador(opDelimitador);
+			} catch (DelimitadorInvalidoException e1) {
+				e1.printStackTrace();
+			}
+				
+			FileParser parser = new FileParser();
+			Character delimiter = opDelimitador.charAt(0);
+			try {
+				parser.parse(Constants.FILE_PATH + "analysisMemory.out", Constants.FILE_PATH + "analysisMemoryTab.out", delimiter, orientation);
+				parser.parse(Constants.FILE_PATH + "analysisTime.out", Constants.FILE_PATH + "analysisTimeTab.out", delimiter, orientation);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+
+        } else if (e.getSource() == reset) {
             String def = "";
             tdelimitador.setText(def);
             coluna.setSelected(false);
             linha.setSelected(false);
-        }
+        }  else if (e.getSource() == linha) {
+			if (linha.isSelected()) {
+		        coluna.setSelected(false);
+				orientation = Constants.ROW_ORIENTATION;
+			}
+		} else if (e.getSource() == coluna) {
+			if (coluna.isSelected()) {
+		        linha.setSelected(false);
+				orientation = Constants.COLUMN_ORIENTATION;
+			}
+		}
 	}
 
-	public String ValidacaoDelimitador(String delimitador_informado) throws DelimitadorInvalidoException{
+	public String ValidacaoDelimitador(String delimitador_informado) throws DelimitadorInvalidoException {
 		if (delimitador_informado == "")
 			throw new DelimitadorInvalidoException("Delimitador Inválido", null);
 			
@@ -149,5 +152,6 @@ public class Menu extends JFrame implements ActionListener {
 	}
 	
 }
+
 
 
